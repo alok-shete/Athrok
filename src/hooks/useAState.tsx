b28d978085322853bec09f533a9c05194c53ab83
Alok Shete <alok.shete@vendekin.com>
2024-05-23 08:04:34 +0530

    resolve set function

--- src/hooks/useAState.tsx
+++ src/hooks/useAState.tsx
@@ -13,6 +13,6 @@ export const useAState = <T,>(
     // Clean up subscription on unmount
     return () => unsubscribe();
   }, [state]);
-
-  return [selectedState, setSelectedState];
+  
+  return [selectedState, state.set];
 };
